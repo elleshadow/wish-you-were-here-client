c6fccca283f8c22b47e805cca45439d576f2a8b9
Eli Davidson <eli@shadowlab.dev>
2022-05-26 19:13:32 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return <h1>This is my React app!</h1>;
+
+  }
+ }
+
+ export default App;
