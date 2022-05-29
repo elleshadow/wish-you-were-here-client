b9e05c05cb4e9beb95de367b1f4431df16e8baaf
Kal <kal.aalrajhi@gmail.com>
2022-05-29 09:18:44 -0600

    Implement live video desktop

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -9,6 +9,12 @@ function Dashboard({ userId }) {
 
     const getVideo = () => {
         navigator.mediaDevices.getUserMedia({ video: { width: 1920, height: 1080 }}) // width and height are scalable TO these params
+        .then(videoFeed => {
+            let video = videoRef.current;
+            video.srcObject = videoFeed;
+            video.play();
+        })
+        .catch(error => console.log(error));
     }
 
     const readURL = (e) => {
@@ -22,31 +28,35 @@ function Dashboard({ userId }) {
         }
     }
 
+    useEffect(() => {
+        getVideo();
+    }, [videoRef]);
+
     return (
-        <>
-        {/* <h1 className='large'>{`Hello ${userId}!`}</h1> */}
-        <section className='live-photo app'>
-            <div className='camera'>
-                <video ref={videoRef}></video>
-                <button>CHEESE!</button>
-            </div>
-            <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
-                <canvas ref={photoRef}></canvas>
-                <button>CLOSE!</button>
-            </div>
+        <section className='image-capture-container'>
+            {/* <h1 className='large'>{`Hello ${userId}!`}</h1> */}
+            <section className='live-photo app'>
+                <div className='camera'>
+                    <video ref={videoRef}></video>
+                    <button>CHEESE!</button>
+                </div>
+                <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+                    <canvas ref={photoRef}></canvas>
+                    <button>CLOSE!</button>
+                </div>
+            </section>
+            <section className='upload-photo'>
+                <input 
+                    // accept="image/png, image/jpeg" - use to limit to .png and .jpeg
+                    accept="image/*" 
+                    id="icon-button-file" 
+                    type="file" 
+                    capture="environment" 
+                    onChange={(e) => readURL(e)}
+                />
+                <img className="preview" src=""/>
+            </section>
         </section>
-        <section className='upload-photo'>
-            <input 
-                // accept="image/png, image/jpeg" - use to limit to .png and .jpeg
-                accept="image/*" 
-                id="icon-button-file" 
-                type="file" 
-                capture="environment" 
-                onChange={(e) => readURL(e)}
-            />
-            <img className="preview" src=""/>
-        </section>
-        </>
     )
 }
 
