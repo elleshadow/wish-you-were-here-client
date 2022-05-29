53cbc4ff322ac44afe2facd274b2ae28e8ed5986
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-05-28 22:16:04 -0600

    Clean up dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,20 +2,6 @@ import React, { useState } from 'react';
 import '../styles/Dashboard.css';
 
 function Dashboard({ userId }) {
-    let image = ""
-
-    const saveImg = (event) => {
-        console.log("TARGET", event.target.value);
-        console.log("TARGET", event.target.id.value);
-    }
-
-    const displayImg = (event) => {
-        event.preventDefault()
-        console.log("EVENT SUBMIT", event)
-        console.log("TARGET SUBMIT", event.target.value);
-        console.log("FILE????", document.getElementById("icon-button-file").files[0])
-        image = document.getElementById("icon-button-file").files[0]
-    }
 
     const readURL = (e) => {
         let input = document.getElementById("icon-button-file")
@@ -28,24 +14,17 @@ function Dashboard({ userId }) {
         }
     }
 
-    
     return (
         <section>
             <h1 className='large'>{`Hello ${userId}!`}</h1>
-            <form onSubmit={(event) => displayImg(event)}>
-                <input 
-                    // accept="image/png, image/jpeg" - use to limit to .png and .jpeg
-                    accept="image/*" 
-                    id="icon-button-file" 
-                    type="file" 
-                    capture="environment" 
-                    onClick={(event) => {saveImg(event)}}
-                    onChange={(e) => readURL(e)}
-                />
-                <button>SUBMIT</button>
-            
-            </form>
-            {/* <img src="../../public/me.png"/> */}
+            <input 
+                // accept="image/png, image/jpeg" - use to limit to .png and .jpeg
+                accept="image/*" 
+                id="icon-button-file" 
+                type="file" 
+                capture="environment" 
+                onChange={(e) => readURL(e)}
+            />
             <img className="preview" src=""/>
         </section>
     )
