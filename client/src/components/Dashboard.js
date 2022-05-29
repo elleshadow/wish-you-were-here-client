24c64a2e728c56de06d12a632d5ca685a788e067
Kal <kal.aalrajhi@gmail.com>
2022-05-29 09:07:02 -0600

    Add live video outline feature

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import '../styles/Dashboard.css';
 
 function Dashboard({ userId }) {
+    const videoRef = useRef(null);
+    const photoRef = useRef(null);
+
+    const [hasPhoto, setHasPhoto] = useState(false);
+
+    const getVideo = () => {
+        navigator.mediaDevices.getUserMedia({ video: { width: 1920, height: 1080 }}) // width and height are scalable TO these params
+    }
 
     const readURL = (e) => {
         let input = document.getElementById("icon-button-file")
@@ -15,8 +23,19 @@ function Dashboard({ userId }) {
     }
 
     return (
-        <section>
-            <h1 className='large'>{`Hello ${userId}!`}</h1>
+        <>
+        {/* <h1 className='large'>{`Hello ${userId}!`}</h1> */}
+        <section className='live-photo app'>
+            <div className='camera'>
+                <video ref={videoRef}></video>
+                <button>CHEESE!</button>
+            </div>
+            <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+                <canvas ref={photoRef}></canvas>
+                <button>CLOSE!</button>
+            </div>
+        </section>
+        <section className='upload-photo'>
             <input 
                 // accept="image/png, image/jpeg" - use to limit to .png and .jpeg
                 accept="image/*" 
@@ -27,6 +46,7 @@ function Dashboard({ userId }) {
             />
             <img className="preview" src=""/>
         </section>
+        </>
     )
 }
 
