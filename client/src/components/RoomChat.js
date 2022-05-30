c500c3bd3d0463bbdc2d5a4fe755102214bccfcc
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-05-30 13:02:41 -0600

    Allow user to type in message box

--- client/src/components/RoomChat.js
+++ client/src/components/RoomChat.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/RoomChat.css'
 import UserList from './UserList';
 
 const RoomChat = ({ connectedUsers }) => {
+
+    const [chatInput, setChatInput] = useState({ message: "" })
+
+    const handleChange = (event) => {
+        setChatInput({ [event.target.name]: event.target.value })
+    }
+
     return (
         <div className="right-side yellow section" contenteditable>
             <div className="right-side-tabs">
@@ -12,8 +19,10 @@ const RoomChat = ({ connectedUsers }) => {
                 <label for="chat">Chat</label>
             </div>
             <div className="chat">
-                <div className="message-container">Message Container</div>
-                <input className="message-box" type="text" placeholder="message..." />
+                <div className="message-container">
+                    Message Container
+                </div>
+                <input className="message-box" name="message" type="text" placeholder="message..." onChange={(event) => handleChange(event)} value={chatInput.message} />
                 <button className="button-send"> Send</button>
                 <section className='socket-list'>
                     <section className='usersListContainer'>
