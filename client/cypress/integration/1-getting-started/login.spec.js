35eac96b150bbdcb5711e51aeb50384560b6ff1a
Kal <kal.aalrajhi@gmail.com>
2022-06-01 16:54:05 -0600

    Add form tests

--- client/cypress/integration/1-getting-started/login.spec.js
+++ client/cypress/integration/1-getting-started/login.spec.js
@@ -1,14 +1,24 @@
 describe('Landing Page', () => {
     beforeEach( () => {
-        cy.visit('http://localhost:3000');
+        cy.visit('http://localhost:3000/?');
     })
 
     it('Should load landing page URL', () => {
-        cy.url().should('eq', 'http://localhost:3000/');
+        cy.url().should('eq', 'http://localhost:3000/?');
     })
 
     // Check what should be displayed
-    it('Should have a title', () => {
-        cy.contains('Turing Cafe Reservations');
+    it('Should have a login form with title, name input, pronoun input and optional email input', () => {
+        cy.get('.form-inner').contains('Login');
+        cy.get('.form-inner').contains('Name');
+        cy.get('.form-inner').contains('Pronouns');
+        cy.get('.form-inner').contains('Email');
+        cy.get('button').contains('Submit');
+    })
+
+    it('User should be able to login with just a name', () => {
+        cy.get('input[name="name"]').type('Bobby-Joe');
+        cy.get('button').click();
     }) 
+
 })
