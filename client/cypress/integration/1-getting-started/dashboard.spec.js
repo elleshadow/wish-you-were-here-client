68a2a3567402e5fca5046c3b9bfcbb4eb020d301
Kal <kal.aalrajhi@gmail.com>
2022-06-01 18:30:25 -0600

    Add chatbox tests

--- client/cypress/integration/1-getting-started/dashboard.spec.js
+++ client/cypress/integration/1-getting-started/dashboard.spec.js
@@ -12,19 +12,42 @@ describe('Dashboard Page', () => {
 
     // Check what should be displayed
     it('Should have a nav bar with logo, title, and logout button.', () => {
-        
+        // Need to test logo and title
+        cy.get('.logout-btn').contains('Log Out');
     })
 
-    it('Should have a chat box with a label, chat window, text field, send button, and connected users', () => {
+    it('User should be able to logout and be redirected to the login screen.', () => {
+        // Need to test logo and title
+        cy.get('.logout-btn').click();
+        cy.url().should('eq', 'http://localhost:3000/?'); // update with implemented router
+        cy.get('.form-inner').contains('Login');
     })
 
-    it('Should have a live camera with a clickable cheese button that takes a photo', () => {
+    it.only('Should have a chat box with a label, chat window, text field, send button, and connected users', () => {
+        cy.get('label[for="chat"]').contains('Chat');
+        cy.get('.message-container').should('exist');
+        cy.get('.message-box').should('exist');
+        cy.get('input[placeholder="message..."]').should('exist');
+        cy.get('.button-send').contains("Send");
+        // Uncomment once server is stable
+        // cy.get('.users-list-container').contains('Bobby-Joe');
+        // cy.get('.users-list-container').contains('Connected Users');
+
     })
 
-    it('Should have camera toggle option, a file picker and an image preview', () => {
+    it('Should be able to send a message in chat and have it appear in chat window.', () => {
     })
 
-    it('When user takes a photo, it should save to a gallery', () => {
+    it('Should have current user shown in connected users area.', () => {
+    })
+
+    it('Should have a live camera with a clickable cheese button that takes a photo.', () => {
+    })
+
+    it('Should have camera toggle option, a file picker and an image preview.', () => {
+    })
+
+    it('When user takes a photo, it should save to a gallery.', () => {
     })
 
 })
