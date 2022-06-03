abfdcf700d4daef7884d49208cdd5121375440f1
Zachary Saile <zwsaile@gmail.com>
2022-06-03 09:14:22 -0600

    Add style changes to send button and nav title

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,6 +5,7 @@ import '../styles/NavBar.css'
 const NavBar = (props) => {
     return (
         <nav className="navBar">
+            <h1 className="app-title">✨Wish You Were Here✨</h1>
             <form onClick={props.logOut}>
                 <button className='logout-btn' type='submit'>Log Out</button>
             </form>
